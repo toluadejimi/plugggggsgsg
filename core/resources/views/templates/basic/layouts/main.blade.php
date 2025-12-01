0578f73785b3b0be1e926fc303dd89043d463113
toluadejimi <toluadejimi@gmail.com>
2025-12-01 14:41:43 +0100

    remove fund added

--- core/resources/views/templates/basic/layouts/main.blade.php
+++ core/resources/views/templates/basic/layouts/main.blade.php
@@ -699,19 +699,6 @@
 </script>
 
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/68fb78cc8f570d1956b4f6ba/1j8b4vooa';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
 
 
 
